42611f515f3701001e77f9f4f79479e8843473f4
Chris Mendez <mendecj812@gmail.com>
2025-08-29 15:26:35 -0500

    update with chapter 3 and 4

---
+++ ch02/intellij/Date.java
@@ -0,0 +1,17 @@
+public class Date {
+    public static void main(String[] args) {
+        String day = "Monday";
+        int date = 25;
+        String month = "August";
+        int year = 2025;
+
+        System.out.println(day);
+        System.out.println(date);
+        System.out.println(month);
+        System.out.println(year);
+
+        System.out.println("---------------------");
+
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+    }
+}
